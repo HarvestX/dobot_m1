9e57d5f087039636b712c77b6e30f1b811b42038
m12watanabe1a <m12watanabe1a@gmail.com>
2020-09-17 00:25:24 +0900

    add ptp subscriber

--- src/m1_controller.cpp
+++ src/m1_controller.cpp
@@ -1,104 +1,208 @@
-#include "DobotDll.h"
 #include <m1_controller/M1Ptp.h>
 #include <ros/ros.h>
 #include <string>
+#include "DobotDll.h"
 
-class M1Controller {
+class M1Controller
+{
 public:
-  M1Controller() : nh_(), pnh_("~") {
+  M1Controller() : nh_(), pnh_("~")
+  {
     pnh_.getParam("port", port_);
+    pnh_.getParam("vel", vel_default_);
+    pnh_.getParam("acc", acc_default_);
     initDobot_();
     sub_ = nh_.subscribe("ptp_command", 1, &M1Controller::ptpCallback, this);
   }
-  ~M1Controller() { check_connection_(DisconnectDobot()); }
+  ~M1Controller()
+  {
+    check_connection_(DisconnectDobot());
+  }
 
-  void ptpCallback(const m1_controller::M1Ptp &msg) { ROS_INFO("hogehoge"); }
+  void ptpCallback(const m1_controller::M1Ptp &msg)
+  {
+    uint64_t lastIndex;
+    // set velocity
+    float vel;
+    PTPCommonParams ptpCommonParams;
+    if (msg.vel == 0.0)
+    {
+      vel = vel_default_;
+    }
+    else
+    {
+      vel = msg.vel;
+    }
+    vel = check_velocity_(vel);
+
+    // set acceleration
+    float acc;
+    if (msg.acc == 0.0)
+    {
+      acc = acc_default_;
+    }
+    else
+    {
+      acc = msg.acc;
+    }
+    acc = check_acceleration_(acc);
+
+    ptpCommonParams.velocityRatio = vel;
+    ptpCommonParams.accelerationRatio = acc;
+
+    PTPCmd cmd;
+    cmd.ptpMode = msg.ptpMode;
+    cmd.x = msg.x;
+    cmd.y = msg.y;
+    cmd.z = msg.z;
+    cmd.r = msg.r;
+
+    /**
+    while (!check_communication_(SetPTPCommonParams(&ptpCommonParams, true, nullptr), "Set PTP Param"))
+    {
+    }
+    **/
+    while (!check_communication_(SetPTPCmd(&cmd, true, nullptr), "Set PTP Cmd"))
+    {
+    }
+  }
 
 private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   ros::Subscriber sub_;
   std::string port_;
+  // TODO: check limit
+  float vel_default_;
+  float vel_min_ = 5.0;
+  float vel_max_ = 100.0;
+  float acc_default_;
+  float acc_min_ = 5.0;
+  float acc_max_ = 100.0;
 
-  void initDobot_() {
-    if (check_connection_(ConnectDobot(port_.c_str(), 115200, 0, 0))) {
-      ROS_INFO("Opened Dobot Communication");
+  void initDobot_()
+  {
+    while (!check_connection_(ConnectDobot(port_.c_str(), 115200, 0, 0)))
+    {
     }
     // set timeout
     uint32_t timeout = 20;
-    check_communication_(SetCmdTimeout(timeout));
+    while (!check_communication_(SetCmdTimeout(timeout), "Set Timeout"))
+    {
+    }
     // clean queue
-    check_communication_(SetQueuedCmdClear());
-    check_communication_(SetQueuedCmdStartExec());
+    while (!check_communication_(SetQueuedCmdClear(), "Queue Clear"))
+    {
+    }
+
+    while (!check_communication_(ClearAllAlarmsState(), "Clear All Alarm"))
+
+      while (!check_communication_(SetQueuedCmdStartExec(), "Start Queue"))
+      {
+      }
     return;
   }
 
-  bool check_connection_(uint8_t status) {
-    switch (status) {
-    case DobotConnect_NoError:
-      ROS_INFO("Dobot Connect No Error");
-      return true;
-    case DobotConnect_NotFound:
-      ROS_ERROR("Dobot Connect Not Found");
-      break;
-    case DobotConnect_Occupied:
-      ROS_ERROR("Dobot Connect Occupied");
-      break;
-    default:
-      ROS_ERROR("Unexpected Return");
-      break;
+  bool check_connection_(uint8_t status)
+  {
+    switch (status)
+    {
+      case DobotConnect_NoError:
+        ROS_INFO("Dobot Connect No Error");
+        return true;
+      case DobotConnect_NotFound:
+        ROS_ERROR("Dobot Connect Not Found");
+        break;
+      case DobotConnect_Occupied:
+        ROS_ERROR("Dobot Connect Occupied");
+        break;
+      default:
+        ROS_ERROR("Unexpected Return");
+        break;
     }
     return false;
   }
 
-  bool check_communication_(uint8_t status) {
-    switch (status) {
-    case DobotCommunicate_NoError:
-      ROS_INFO("Dobot Communication No Error");
-      return true;
-    case DobotCommunicate_BufferFull:
-      ROS_ERROR("Dobot Communication Buffer Full");
-      break;
-    case DobotCommunicate_Timeout:
-      ROS_ERROR("Dobot Communication Timeout");
-      break;
-    case DobotCommunicate_InvalidParams:
-      ROS_ERROR("Dobot Communication Invalid Params");
-      break;
-    default:
-      ROS_ERROR("Unexpected Return");
-      break;
+  bool check_communication_(uint8_t status, std::string process_name)
+  {
+    switch (status)
+    {
+      case DobotCommunicate_NoError:
+        ROS_INFO("Dobot Communication No Error: %s", process_name.c_str());
+        return true;
+      case DobotCommunicate_BufferFull:
+        ROS_ERROR("Dobot Communication Buffer Full: %s", process_name.c_str());
+        break;
+      case DobotCommunicate_Timeout:
+        ROS_ERROR("Dobot Communication Timeout: %s", process_name.c_str());
+        break;
+      case DobotCommunicate_InvalidParams:
+        ROS_ERROR("Dobot Communication Invalid Params: %s", process_name.c_str());
+        break;
+      default:
+        ROS_ERROR("Unexpected Return: %s", process_name.c_str());
+        break;
     }
     return false;
+  }
+
+  float check_velocity_(float vel)
+  {
+    if (vel < vel_min_)
+    {
+      ROS_WARN("velocity modified to minimum value: %f", vel_min_);
+      vel = vel_min_;
+    }
+    else if (vel > vel_max_)
+    {
+      ROS_WARN("velocity modified to maximum value: %f", vel_max_);
+      vel = vel_max_;
+    }
+    return vel;
+  }
+
+  float check_acceleration_(float acc)
+  {
+    if (acc < acc_min_)
+    {
+      ROS_WARN("acceleration modified to minimum value: %f", acc_min_);
+      acc = acc_min_;
+    }
+    else if (acc > acc_max_)
+    {
+      ROS_WARN("acceleration modified to maximum value: %f", acc_max_);
+      acc = acc_max_;
+    }
+    return acc;
   }
 };
 
-void cpcmd(double x, double y, double z, double r) {
+void cpcmd(double x, double y, double z, double r)
+{
   CPCmd cmd;
   uint64_t lastIndex;
   bool ret;
 
-  cmd.cpMode = 1; // movel:2 movej:1
-  cmd.x = x;
-  cmd.y = y;
-  cmd.z = z;
-
-  while (SetQueuedCmdClear() != DobotCommunicate_NoError) {
+  while (SetQueuedCmdClear() != DobotCommunicate_NoError)
+  {
   }
-  while (SetCPCmd(&cmd, true, &lastIndex) != DobotCommunicate_NoError) {
+  while (SetCPCmd(&cmd, true, &lastIndex) != DobotCommunicate_NoError)
+  {
   }
 
   uint64_t currentIndex;
-  while (1) {
-    while (GetQueuedCmdCurrentIndex(&currentIndex) !=
-           DobotCommunicate_NoError) {
+  while (1)
+  {
+    while (GetQueuedCmdCurrentIndex(&currentIndex) != DobotCommunicate_NoError)
+    {
     }
     if (lastIndex <= currentIndex)
       break;
   }
 }
 
-int tmp() {
+int tmp()
+{
   uint64_t queuedCmdIndex;
   uint64_t lastIndex;
 
@@ -106,9 +210,12 @@ int tmp() {
   // ####### need!!! ################
   // sudo chmod a+rw /dev/ttyUSB0
   // ################################
-  if (ConnectDobot("/dev/ttyUSB0", 115200, 0, 0) == DobotCommunicate_NoError) {
+  if (ConnectDobot("/dev/ttyUSB0", 115200, 0, 0) == DobotCommunicate_NoError)
+  {
     ROS_INFO("Opened Dobot Communication");
-  } else {
+  }
+  else
+  {
     ROS_ERROR("Invalid port name or Dobot is occupied by other application or "
               "something else :)");
   }
@@ -118,17 +225,20 @@ int tmp() {
 
   // Set some Parameter and Initialisation
   // First clear the command queue
-  if (SetQueuedCmdClear() == DobotCommunicate_NoError) {
+  if (SetQueuedCmdClear() == DobotCommunicate_NoError)
+  {
     ROS_INFO("CMD Queue Cleared");
   }
   // and start the exec queue
-  if (SetQueuedCmdStartExec() == DobotCommunicate_NoError) {
+  if (SetQueuedCmdStartExec() == DobotCommunicate_NoError)
+  {
     ROS_INFO("CMD Queue Started");
   }
 
   // while (SetHOMEWithSwitch(0, true, nullptr) != DobotCommunicate_NoError) {}
   uint8_t i;
-  for (i = 0; i < 1; i++) {
+  for (i = 0; i < 1; i++)
+  {
     cpcmd(400, 0, 200, 0);
     cpcmd(250, 50, 200, 0);
     cpcmd(250, -50, 200, 0);
@@ -140,7 +250,8 @@ int tmp() {
   return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "m1_controller");
   M1Controller m1_controller;
   ros::spin();
