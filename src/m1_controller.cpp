fcb1bafa72a7e0d15df8723f4b0c99c7ff0d1486
m12watanabe1a <m12watanabe1a@gmail.com>
2020-09-23 22:17:09 +0900

    remove garbage code

--- src/m1_controller.cpp
+++ src/m1_controller.cpp
@@ -306,68 +306,6 @@ private:
   }
 };
 
-void cpcmd(double x, double y, double z, double r) {
-  CPCmd cmd;
-  uint64_t lastIndex;
-  bool ret;
-
-  while (SetQueuedCmdClear() != DobotCommunicate_NoError) {
-  }
-  while (SetCPCmd(&cmd, true, &lastIndex) != DobotCommunicate_NoError) {
-  }
-
-  uint64_t currentIndex;
-  while (1) {
-    while (GetQueuedCmdCurrentIndex(&currentIndex) !=
-           DobotCommunicate_NoError) {
-    }
-    if (lastIndex <= currentIndex)
-      break;
-  }
-}
-
-int tmp() {
-  uint64_t queuedCmdIndex;
-  uint64_t lastIndex;
-
-  // Connect to Dobot
-  // ####### need!!! ################
-  // sudo chmod a+rw /dev/ttyUSB0
-  // ################################
-  if (ConnectDobot("/dev/ttyUSB0", 115200, 0, 0) == DobotCommunicate_NoError) {
-    ROS_INFO("Opened Dobot Communication");
-  } else {
-    ROS_ERROR("Invalid port name or Dobot is occupied by other application or "
-              "something else :)");
-  }
-  // Set Commuinication timeout in ms
-  uint32_t timeout = 20;
-  SetCmdTimeout(timeout);
-
-  // Set some Parameter and Initialisation
-  // First clear the command queue
-  if (SetQueuedCmdClear() == DobotCommunicate_NoError) {
-    ROS_INFO("CMD Queue Cleared");
-  }
-  // and start the exec queue
-  if (SetQueuedCmdStartExec() == DobotCommunicate_NoError) {
-    ROS_INFO("CMD Queue Started");
-  }
-
-  // while (SetHOMEWithSwitch(0, true, nullptr) != DobotCommunicate_NoError) {}
-  uint8_t i;
-  for (i = 0; i < 1; i++) {
-    cpcmd(400, 0, 200, 0);
-    cpcmd(250, 50, 200, 0);
-    cpcmd(250, -50, 200, 0);
-  }
-
-  // SetQueuedCmdStopExec();
-
-  DisconnectDobot();
-  return 0;
-}
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, "m1_controller");
   M1Controller m1_controller;
