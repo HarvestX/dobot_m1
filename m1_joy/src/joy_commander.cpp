af013c20efb0ed8ef299a54c5e8ea6e715365998
m12watanabe1a <m12watanabe1a@gmail.com>
2020-09-25 23:08:19 +0900

    add r axis attribution and isJoint attribution

--- m1_joy/src/joy_commander.cpp
+++ m1_joy/src/joy_commander.cpp
@@ -5,7 +5,8 @@
 
 class JoyCommander {
 public:
-  JoyCommander() : nh_() {
+  JoyCommander() : nh_(), pnh_() {
+    pnh_.getParam("isJoint", isJoint_);
     jog_pub_ = nh_.advertise<m1_msgs::M1Jog>("jog_cmd", 10);
     joy_sub_ = nh_.subscribe("/joy", 1, &JoyCommander::joyCallback, this);
   }
@@ -13,19 +14,21 @@ public:
   void joyCallback(const sensor_msgs::Joy &joy_msg) {
 
     m1_msgs::M1Jog jog_msg;
-    jog_msg.isJoint = 0;
+    jog_msg.isJoint = isJoint_;
     jog_msg.jogCmd = 0;
 
     float x = joy_msg.axes[1]; // L stick horizontal
     float y = joy_msg.axes[0]; // L stick vertical
     float z = joy_msg.axes[4]; // R stick vertical
+    float r = joy_msg.axes[3]; // R stick horizontal
 
     float x_abs = abs(x);
     float y_abs = abs(y);
     float z_abs = abs(z);
+    float r_abs = abs(r);
 
-    float vel = std::max({x_abs, y_abs, z_abs});
-    if (vel > 0.3) {
+    float vel = std::max({x_abs, y_abs, z_abs, r_abs});
+    if (vel > 0.2) {
       if (x_abs >= vel) {
         if (x > 0) {
           jog_msg.jogCmd = 1; // X+
@@ -46,6 +49,15 @@ public:
         } else {
           jog_msg.jogCmd = 6; // Z-
         }
+      } else if (r_abs >= vel) {
+        // Move Joint directly to avoid axis limitation
+        jog_msg.isJoint = 1;
+        if (r > 0) {
+          jog_msg.jogCmd = 7; // R+
+
+        } else {
+          jog_msg.jogCmd = 8; // R-
+        }
       }
       jog_msg.vel = vel * 100.0;
       jog_msg.acc = 100.0;
@@ -55,8 +67,10 @@ public:
 
 private:
   ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
   ros::Publisher jog_pub_;
   ros::Subscriber joy_sub_;
+  bool isJoint_ = true;
 };
 
 int main(int argc, char **argv) {
