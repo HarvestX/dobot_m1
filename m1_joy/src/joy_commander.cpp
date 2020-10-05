1be53da96172514fddbd14249a65afe03b9855d6
m12watanabe1a <m12watanabe1a@gmail.com>
2020-10-05 18:17:21 +0900

    clean code

--- m1_joy/src/joy_commander.cpp
+++ m1_joy/src/joy_commander.cpp
@@ -1,81 +1,71 @@
-#include <algorithm>
-#include <m1_msgs/M1Jog.h>
-#include <ros/ros.h>
-#include <sensor_msgs/Joy.h>
+#include "joy_commander.h"
 
-class JoyCommander {
-public:
-  JoyCommander() : nh_(), pnh_() {
-    pnh_.getParam("isJoint", isJoint_);
-    jog_pub_ = nh_.advertise<m1_msgs::M1Jog>("jog_cmd", 10);
-    joy_sub_ = nh_.subscribe("/joy", 1, &JoyCommander::joyCallback, this);
-  }
+namespace dobot_m1 {
+JoyCommander::JoyCommander() : nh_(), pnh_() {
+  pnh_.getParam("isJoint", isJoint_);
+  jog_pub_ = nh_.advertise<m1_msgs::M1Jog>("jog_cmd", 10);
+  joy_sub_ = nh_.subscribe("/joy", 1, &JoyCommander::joyCallback, this);
+}
 
-  void joyCallback(const sensor_msgs::Joy &joy_msg) {
+void JoyCommander::joyCallback(const sensor_msgs::Joy &joy_msg) {
 
-    m1_msgs::M1Jog jog_msg;
-    jog_msg.isJoint = isJoint_;
-    jog_msg.jogCmd = 0;
+  m1_msgs::M1Jog jog_msg;
+  jog_msg.isJoint = isJoint_;
+  jog_msg.jogCmd = 0;
 
-    float x = joy_msg.axes[1]; // L stick horizontal
-    float y = joy_msg.axes[0]; // L stick vertical
-    float z = joy_msg.axes[4]; // R stick vertical
-    float r = joy_msg.axes[3]; // R stick horizontal
+  float x = joy_msg.axes[1]; // L stick horizontal
+  float y = joy_msg.axes[0]; // L stick vertical
+  float z = joy_msg.axes[4]; // R stick vertical
+  float r = joy_msg.axes[3]; // R stick horizontal
 
-    float x_abs = abs(x);
-    float y_abs = abs(y);
-    float z_abs = abs(z);
-    float r_abs = abs(r);
+  float x_abs = abs(x);
+  float y_abs = abs(y);
+  float z_abs = abs(z);
+  float r_abs = abs(r);
 
-    float vel = std::max({x_abs, y_abs, z_abs, r_abs});
-    if (vel > 0.4) {
-      if (x_abs >= vel) {
-        if (x > 0) {
-          jog_msg.jogCmd = 1; // X+
-        } else {
-          jog_msg.jogCmd = 2; // X-
-        }
-      } else if (y_abs >= vel) {
-        if (y > 0) {
-          jog_msg.jogCmd = 3; // Y+
-        } else {
-          jog_msg.jogCmd = 4; // Y-
-        }
-      } else if (z_abs >= vel) {
-        // Move Joint directly to avoid axis limitation
-        jog_msg.isJoint = 1;
-        if (z > 0) {
-          jog_msg.jogCmd = 5; // Z+
-        } else {
-          jog_msg.jogCmd = 6; // Z-
-        }
-      } else if (r_abs >= vel) {
-        // Move Joint directly to avoid axis limitation
-        jog_msg.isJoint = 1;
-        if (r > 0) {
-          jog_msg.jogCmd = 7; // R+
-
-        } else {
-          jog_msg.jogCmd = 8; // R-
-        }
+  float vel = std::max({x_abs, y_abs, z_abs, r_abs});
+  if (vel > 0.4) {
+    if (x_abs >= vel) {
+      if (x > 0) {
+        jog_msg.jogCmd = 1; // X+
+      } else {
+        jog_msg.jogCmd = 2; // X-
       }
-      jog_msg.vel = round(vel * 100.0);
-      jog_msg.acc = 100.0;
-    }
-    jog_pub_.publish(jog_msg);
-  }
+    } else if (y_abs >= vel) {
+      if (y > 0) {
+        jog_msg.jogCmd = 3; // Y+
+      } else {
+        jog_msg.jogCmd = 4; // Y-
+      }
+    } else if (z_abs >= vel) {
+      // Move Joint directly to avoid axis limitation
+      jog_msg.isJoint = 1;
+      if (z > 0) {
+        jog_msg.jogCmd = 5; // Z+
+      } else {
+        jog_msg.jogCmd = 6; // Z-
+      }
+    } else if (r_abs >= vel) {
+      // Move Joint directly to avoid axis limitation
+      jog_msg.isJoint = 1;
+      if (r > 0) {
+        jog_msg.jogCmd = 7; // R+
 
-private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::Publisher jog_pub_;
-  ros::Subscriber joy_sub_;
-  bool isJoint_ = true;
-};
+      } else {
+        jog_msg.jogCmd = 8; // R-
+      }
+    }
+    jog_msg.vel = round(vel * 100.0);
+    jog_msg.acc = 100.0;
+  }
+  jog_pub_.publish(jog_msg);
+}
+
+} // namespace dobot_m1
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "joy_commander");
-  JoyCommander joy_commander;
+  dobot_m1::JoyCommander joy_commander;
   ros::Rate loop_rate(60);
   while (ros::ok()) {
     ros::spinOnce();
---
+++ m1_joy/src/joy_commander.h
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <m1_msgs/M1Jog.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+namespace dobot_m1 {
+class JoyCommander {
+public:
+  JoyCommander();
+  void joyCallback(const sensor_msgs::Joy &joy_msg);
+
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Publisher jog_pub_;
+  ros::Subscriber joy_sub_;
+  bool isJoint_ = true;
+};
+} // namespace dobot_m1
