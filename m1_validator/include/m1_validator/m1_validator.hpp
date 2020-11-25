55124cae7ae0c43511ed37a204c8d8baa284acb3
m12watanabe1a <m12watanabe1a@gmail.com>
2020-11-25 21:45:52 +0900

    add validator

--- m1_validator/include/m1_validator/m1_validator.hpp
+++ m1_validator/include/m1_validator/m1_validator.hpp
@@ -2,6 +2,7 @@
 #define ROS_CONTROL__M1_VALIDATOR_H
 
 #include <actionlib/server/simple_action_server.h>
+#include <cmath>
 #include <m1_msgs/M1Cp.h>
 #include <m1_msgs/M1Jog.h>
 #include <m1_msgs/M1Ptp.h>
@@ -19,6 +20,13 @@ public:
   M1Validator();
   ~M1Validator();
   void actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal);
+  bool isValidPosition(float x, float y, float z);
+  bool checkPosition(float x, float y, float z);
+  bool checkJoint(float x, float y);
+  float deg2rad(float degree);
+  bool is_left_hand = true;
+  const float l1 = 200.0;
+  const float l2 = 200.0;
 };
 } // namespace dobot_m1
 #endif
