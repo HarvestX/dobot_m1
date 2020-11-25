2baaaa8dcb8c042619a4e80e56b2773e86f3c0ec
m12watanabe1a <m12watanabe1a@gmail.com>
2020-11-25 20:36:41 +0900

    format documents

--- m1_validator/include/m1_validator/m1_validator.hpp
+++ m1_validator/include/m1_validator/m1_validator.hpp
@@ -1,25 +1,24 @@
 #ifndef ROS_CONTROL__M1_VALIDATOR_H
 #define ROS_CONTROL__M1_VALIDATOR_H
 
-#include<ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
 #include <m1_msgs/M1Cp.h>
 #include <m1_msgs/M1Jog.h>
 #include <m1_msgs/M1Ptp.h>
 #include <m1_msgs/M1PtpAction.h>
-#include <actionlib/server/simple_action_server.h>
+#include <ros/ros.h>
 
 namespace dobot_m1 {
-  class M1Validator
-  {
-  private:
+class M1Validator {
+private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   actionlib::SimpleActionServer<m1_msgs::M1PtpAction> ptp_as_;
 
-  public:
-    M1Validator();
-    ~M1Validator();
+public:
+  M1Validator();
+  ~M1Validator();
   void actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal);
-  };
-}
+};
+} // namespace dobot_m1
 #endif
--- m1_validator/src/m1_validator.cpp
+++ m1_validator/src/m1_validator.cpp
@@ -1,14 +1,13 @@
 #include <m1_validator.hpp>
 
-namespace dobot_m1{
+namespace dobot_m1 {
 
 M1Validator::M1Validator()
     : ptp_as_(nh_, "m1_ptp_action",
               boost::bind(&M1Validator::actionPtpCallback, this, _1), false),
-      nh_(), pnh_("~") {
-      }
+      nh_(), pnh_("~") {}
 
-M1Validator::~M1Validator(){}
+M1Validator::~M1Validator() {}
 
 void M1Validator::actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal) {
   m1_msgs::M1PtpResult result;
@@ -18,10 +17,9 @@ void M1Validator::actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal) {
   return;
 }
 
-}
+} // namespace dobot_m1
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ros::init(argc, argv, "m1_validator");
   dobot_m1::M1Validator my_validator;
   ros::spin();
