405828e0624905a49a52ff541add6bde575b45a3
m12watanabe1a <m12watanabe1a@gmail.com>
2021-02-23 21:01:58 +0900

    add service basics and remove action server

--- m1_controller/include/m1_controller/dobot_m1.hpp
+++ m1_controller/include/m1_controller/dobot_m1.hpp
@@ -1,63 +1,64 @@
+#pragma once
 
-#ifndef ROS_CONTROL__DOBOT_M1_H
-#define ROS_CONTROL__DOBOT_M1_H
-
-#include <actionlib/server/simple_action_server.h>
 #include <m1_msgs/M1Cp.h>
 #include <m1_msgs/M1Jog.h>
 #include <m1_msgs/M1Ptp.h>
-#include <m1_msgs/M1PtpAction.h>
+#include <m1_msgs/M1PtpService.h>
+#include <m1_msgs/M1CpService.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <string>
 
-namespace dobot_api {
+namespace dobot_api
+{
 #include "DobotDll.h"
 }
 
-namespace dobot_m1 {
-class DobotM1 {
-public:
-  DobotM1();
-  ~DobotM1();
+namespace dobot_m1
+{
+  const float MIN_VEL = 5.0;
+  const float MAX_VEL = 100.0;
 
-  void connectDobot();
-  void initDobot();
-  void timerCallback(const ros::TimerEvent &);
-  void ptpCallback(const m1_msgs::M1Ptp &msg);
-  void cpCallback(const m1_msgs::M1Cp &msg);
-  void jogCallback(const m1_msgs::M1Jog &msg);
-  void actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal);
-  void homing();
+  const float MIN_ACC = 5.0;
+  const float MAX_ACC = 100.0;
 
-private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::Publisher joint_pub_;
-  ros::Subscriber ptp_sub_;
-  ros::Subscriber cp_sub_;
-  ros::Subscriber jog_sub_;
-  ros::Timer timer_;
-  actionlib::SimpleActionServer<m1_msgs::M1PtpAction> ptp_as_;
+  class DobotM1
+  {
+  public:
+    DobotM1();
+    ~DobotM1();
 
-  // std::string port_ = "/dev/ttyUSB0";
-  std::string port_ = "192.168.100.159";
+    void connectDobot();
+    void initDobot();
+    void timerCallback(const ros::TimerEvent &);
+    void ptpCallback(const m1_msgs::M1Ptp &msg);
+    void cpCallback(const m1_msgs::M1Cp &msg);
+    void jogCallback(const m1_msgs::M1Jog &msg);
+    void homing();
 
-  float vel_default_;
-  const float vel_min_ = 5.0;
-  const float vel_max_ = 100.0;
-  float acc_default_;
-  const float acc_min_ = 5.0;
-  const float acc_max_ = 100.0;
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Publisher joint_pub_;
+    ros::Subscriber ptp_sub_;
+    ros::Subscriber cp_sub_;
+    ros::Subscriber jog_sub_;
+    ros::Timer timer_;
 
-  bool check_connection_(uint8_t status);
-  bool check_communication_(uint8_t status, std::string process_name);
-  float check_velocity_(float vel);
-  float check_acceleration_(float acc);
-  void checkAlarm_();
-  bool assertAlarm_();
-  void publishAlarm_(dobot_api::alarmState alarmstate);
-};
+    // services
+    ros::ServiceServer ptp_service_;
+    ros::ServiceServer cp_service_;
+    bool PtpServiceCallback_(m1_msgs::M1PtpServiceRequest &req, m1_msgs::M1PtpServiceResponse &res);
+    bool CpServiceCallback_(m1_msgs::M1CpServiceRequest &req, m1_msgs::M1CpServiceResponse &res);
+
+    std::string port_;
+
+    bool check_connection_(uint8_t status);
+    bool check_communication_(uint8_t status, std::string process_name);
+    float check_velocity_(float vel);
+    float check_acceleration_(float acc);
+    void checkAlarm_();
+    bool assertAlarm_();
+    void publishAlarm_(dobot_api::alarmState alarmstate);
+  };
 } // namespace dobot_m1
-
-#endif
--- m1_controller/src/dobot_m1.cpp
+++ m1_controller/src/dobot_m1.cpp
@@ -1,358 +1,351 @@
 #include <dobot_m1.hpp>
 
-namespace dobot_m1 {
-DobotM1::DobotM1()
-    : ptp_as_(nh_, "m1_ptp_action",
-              boost::bind(&DobotM1::actionPtpCallback, this, _1), false),
-      nh_(), pnh_("~") {
-  pnh_.getParam("port", port_);
-  pnh_.getParam("vel", vel_default_);
-  pnh_.getParam("acc", acc_default_);
-  initDobot();
-  joint_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_state", 10);
-  timer_ = nh_.createTimer(ros::Duration(1), &DobotM1::timerCallback, this);
-  ptp_sub_ = nh_.subscribe("ptp_cmd", 1, &DobotM1::ptpCallback, this);
-  cp_sub_ = nh_.subscribe("cp_cmd", 1, &DobotM1::cpCallback, this);
-  jog_sub_ = nh_.subscribe("jog_cmd", 1, &DobotM1::jogCallback, this);
-  ptp_as_.start();
-}
+namespace dobot_m1
+{
+  DobotM1::DobotM1()
+      : nh_(), pnh_("~")
+  {
+    pnh_.param<std::string>("port", port_, "192.168.100.159");
 
-DobotM1::~DobotM1() { dobot_api::DisconnectDobot(); }
-
-void DobotM1::connectDobot() {
-  ROS_INFO("connecting to [%s]", DobotM1::port_.c_str());
-  while (!check_connection_(
-      dobot_api::ConnectDobot(port_.c_str(), 115200, nullptr, nullptr))) {
-  }
-}
-
-void DobotM1::initDobot() {
-  connectDobot();
-
-  // set timeout
-  uint32_t timeout = 50;
-  while (
-      !check_communication_(dobot_api::SetCmdTimeout(timeout), "Set Timeout")) {
-  }
-  // clean queue
-  while (!check_communication_(dobot_api::SetQueuedCmdClear(), "Queue Clear")) {
+    initDobot();
+    joint_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_state", 10);
+    timer_ = nh_.createTimer(ros::Duration(1), &DobotM1::timerCallback, this);
+    ptp_sub_ = nh_.subscribe("ptp_cmd", 1, &DobotM1::ptpCallback, this);
+    cp_sub_ = nh_.subscribe("cp_cmd", 1, &DobotM1::cpCallback, this);
+    jog_sub_ = nh_.subscribe("jog_cmd", 1, &DobotM1::jogCallback, this);
+    ptp_service_ = nh_.advertiseService("ptp_service", &DobotM1::PtpServiceCallback_, this);
+    cp_service_ = nh_.advertiseService("cp_service", &DobotM1::CpServiceCallback_, this);
   }
 
-  while (!check_communication_(dobot_api::ClearAllAlarmsState(),
-                               "Clear All Alarm")) {
-  }
+  DobotM1::~DobotM1() { dobot_api::DisconnectDobot(); }
 
-  while (!check_communication_(dobot_api::SetQueuedCmdStartExec(),
-                               "Start Queue")) {
-  }
-
-  return;
-}
-
-void DobotM1::timerCallback(const ros::TimerEvent &) {
-  checkAlarm_();
-
-  // Publish Position
-  dobot_api::Pose pose;
-  if (GetPose(&pose) != dobot_api::DobotCommunicate_NoError) {
-    return;
-  };
-
-  sensor_msgs::JointState joint_msg;
-  joint_msg.header.stamp = ros::Time::now();
-  joint_msg.name.resize(4);
-  joint_msg.position.resize(4);
-  joint_msg.name[0] = "joint1";
-  joint_msg.position[0] = (float)pose.jointAngle[0];
-  joint_msg.name[1] = "joint2";
-  joint_msg.position[1] = (float)pose.jointAngle[1];
-  joint_msg.name[2] = "joint3";
-  joint_msg.position[2] = (float)pose.jointAngle[2];
-  joint_msg.name[3] = "joint4";
-  joint_msg.position[3] = (float)pose.jointAngle[3];
-  joint_pub_.publish(joint_msg);
-}
-
-void DobotM1::ptpCallback(const m1_msgs::M1Ptp &msg) {
-  uint64_t lastIndex;
-  // set velocity
-  float vel;
-  if (msg.vel == 0.0) {
-    vel = vel_default_;
-  } else {
-    vel = msg.vel;
-  }
-  vel = check_velocity_(vel);
-
-  // set acceleration
-  float acc;
-  if (msg.acc == 0.0) {
-    acc = acc_default_;
-  } else {
-    acc = msg.acc;
-  }
-  acc = check_acceleration_(acc);
-
-  dobot_api::PTPCommonParams ptpCommonParams;
-  ptpCommonParams.velocityRatio = vel;
-  ptpCommonParams.accelerationRatio = acc;
-
-  dobot_api::PTPCmd cmd;
-  cmd.ptpMode = msg.ptpMode;
-  cmd.x = msg.x;
-  cmd.y = msg.y;
-  cmd.z = msg.z;
-  cmd.r = msg.r;
-
-  // Start session with dobot
-  while (
-      !check_communication_(dobot_api::SetArmOrientation(
-                                dobot_api::LeftyArmOrientation, true, nullptr),
-                            "Set Arm Orientation")) {
-  }
-  while (!check_communication_(
-      SetPTPCommonParams(&ptpCommonParams, true, nullptr), "Set PTP Param")) {
-  }
-  while (!check_communication_(dobot_api::SetPTPCmd(&cmd, true, nullptr),
-                               "Set PTP Cmd")) {
-  }
-  checkAlarm_();
-}
-
-void DobotM1::cpCallback(const m1_msgs::M1Cp &msg) {
-  uint64_t lastIndex;
-  // set velocity
-  float vel;
-  if (msg.vel == 0.0) {
-    vel = vel_default_;
-  } else {
-    vel = msg.vel;
-  }
-  vel = check_velocity_(vel);
-
-  // set acceleration
-  float acc;
-  if (msg.acc == 0.0) {
-    acc = acc_default_;
-  } else {
-    acc = msg.acc;
-  }
-  acc = check_acceleration_(acc);
-
-  dobot_api::CPParams cpParams;
-  cpParams.juncitionVel = vel;
-  cpParams.planAcc = acc;
-
-  dobot_api::CPCmd cmd;
-  cmd.cpMode = msg.cpMode;
-  cmd.x = msg.x;
-  cmd.y = msg.y;
-  cmd.z = msg.z;
-
-  // Start session with dobot
-  // Cp command only works on RightyArmOrientation
-  while (
-      !check_communication_(dobot_api::SetArmOrientation(
-                                dobot_api::RightyArmOrientation, true, nullptr),
-                            "Set Arm Orientation")) {
-  }
-  while (!check_communication_(SetCPParams(&cpParams, true, nullptr),
-                               "Set CP Param")) {
-  }
-  while (!check_communication_(SetCPCmd(&cmd, true, nullptr), "Set CP Cmd")) {
-  }
-  checkAlarm_();
-}
-
-void DobotM1::jogCallback(const m1_msgs::M1Jog &msg) {
-  // set velocity
-  float vel;
-  if (msg.vel == 0.0) {
-    vel = vel_default_;
-  } else {
-    vel = msg.vel;
-  }
-  vel = check_velocity_(vel);
-
-  // set acceleration
-  float acc;
-  if (msg.acc == 0.0) {
-    acc = acc_default_;
-  } else {
-    acc = msg.acc;
-  }
-  acc = check_acceleration_(acc);
-
-  dobot_api::JOGCommonParams jogCommonParams;
-  jogCommonParams.velocityRatio = vel;
-  jogCommonParams.accelerationRatio = acc;
-
-  dobot_api::JOGCmd cmd;
-  cmd.isJoint = msg.isJoint;
-  cmd.cmd = msg.jogCmd;
-
-  // Start session with dobot
-  while (!check_communication_(
-      SetJOGCommonParams(&jogCommonParams, true, nullptr), "Set JOG Param")) {
-  }
-  while (!check_communication_(dobot_api::SetJOGCmd(&cmd, false, nullptr),
-                               "Set JOG Cmd")) {
-  }
-  checkAlarm_();
-}
-
-void DobotM1::actionPtpCallback(const m1_msgs::M1PtpGoalConstPtr &goal) {
-  m1_msgs::M1PtpResult result;
-  result.id = goal->id;
-
-  dobot_api::PTPCommonParams ptpCommonParams;
-  ptpCommonParams.velocityRatio = goal->m1_ptp.vel;
-  ptpCommonParams.accelerationRatio = goal->m1_ptp.acc;
-
-  dobot_api::PTPCmd cmd;
-  cmd.ptpMode = goal->m1_ptp.ptpMode;
-  cmd.x = goal->m1_ptp.x;
-  cmd.y = goal->m1_ptp.y;
-  cmd.z = goal->m1_ptp.z;
-
-  // Start session with dobot
-  while (
-      !check_communication_(dobot_api::SetArmOrientation(
-                                dobot_api::LeftyArmOrientation, true, nullptr),
-                            "Set Arm Orientation")) {
-  }
-  while (!check_communication_(
-      SetPTPCommonParams(&ptpCommonParams, true, nullptr), "Set PTP Param")) {
-  }
-  while (!check_communication_(dobot_api::SetPTPCmd(&cmd, true, nullptr),
-                               "Set PTP Cmd")) {
-  }
-
-  result.status.data = DobotM1::assertAlarm_();
-  DobotM1::ptp_as_.setSucceeded(result);
-  return;
-}
-
-void DobotM1::homing() {
-  // clean queue
-  while (!check_communication_(dobot_api::SetQueuedCmdClear(), "Queue Clear")) {
-  }
-
-  while (!check_communication_(dobot_api::SetQueuedCmdStartExec(),
-                               "Start Queue")) {
-  }
-
-  uint64_t lastIndex;
-  uint64_t currentIndex;
-
-  while (!check_communication_(
-      dobot_api::SetHOMEWithSwitch(0, true, &lastIndex), "Homing")) {
-  }
-  while (1) {
-    while (dobot_api::GetQueuedCmdCurrentIndex(&currentIndex) !=
-           dobot_api::DobotCommunicate_NoError) {
+  void DobotM1::connectDobot()
+  {
+    ROS_INFO("connecting to [%s]", DobotM1::port_.c_str());
+    while (!check_connection_(
+        dobot_api::ConnectDobot(port_.c_str(), 115200, nullptr, nullptr)))
+    {
     }
-    if (lastIndex <= currentIndex)
-      break;
   }
 
-  ROS_INFO("Homing Successfully Finished!");
-  return;
-}
+  void DobotM1::initDobot()
+  {
+    connectDobot();
 
-bool DobotM1::check_connection_(uint8_t status) {
-  switch (status) {
-  case dobot_api::DobotConnect_NoError:
-    return true;
-  case dobot_api::DobotConnect_NotFound:
-    ROS_ERROR("Dobot Connect Not Found");
-    break;
-  case dobot_api::DobotConnect_Occupied:
-    ROS_ERROR("Dobot Connect Occupied");
-    break;
-  default:
-    ROS_ERROR("Unexpected Return");
-    break;
-  }
-  return false;
-}
+    // set timeout
+    uint32_t timeout = 50;
+    while (
+        !check_communication_(dobot_api::SetCmdTimeout(timeout), "Set Timeout"))
+    {
+    }
+    // clean queue
+    while (!check_communication_(dobot_api::SetQueuedCmdClear(), "Queue Clear"))
+    {
+    }
 
-bool DobotM1::check_communication_(uint8_t status, std::string process_name) {
-  switch (status) {
-  case dobot_api::DobotCommunicate_NoError:
-    ROS_INFO("%s", process_name.c_str());
-    return true;
-  case dobot_api::DobotCommunicate_BufferFull:
-    ROS_ERROR("Dobot Communication Buffer Full: %s", process_name.c_str());
-    break;
-  case dobot_api::DobotCommunicate_Timeout:
-    ROS_ERROR("Dobot Communication Timeout: %s", process_name.c_str());
-    break;
-  case dobot_api::DobotCommunicate_InvalidParams:
-    ROS_ERROR("Dobot Communication Invalid Params: %s", process_name.c_str());
-    break;
-  default:
-    ROS_ERROR("Unexpected Return: %s", process_name.c_str());
-    break;
-  }
-  return false;
-}
+    while (!check_communication_(dobot_api::ClearAllAlarmsState(),
+                                 "Clear All Alarm"))
+    {
+    }
 
-float DobotM1::check_velocity_(float vel) {
-  if (vel < vel_min_) {
-    ROS_WARN("velocity modified to minimum value: %f", vel_min_);
-    vel = vel_min_;
-  } else if (vel > vel_max_) {
-    ROS_WARN("velocity modified to maximum value: %f", vel_max_);
-    vel = vel_max_;
-  }
-  return vel;
-}
+    while (!check_communication_(dobot_api::SetQueuedCmdStartExec(),
+                                 "Start Queue"))
+    {
+    }
 
-float DobotM1::check_acceleration_(float acc) {
-  if (acc < acc_min_) {
-    ROS_WARN("acceleration modified to minimum value: %f", acc_min_);
-    acc = acc_min_;
-  } else if (acc > acc_max_) {
-    ROS_WARN("acceleration modified to maximum value: %f", acc_max_);
-    acc = acc_max_;
-  }
-  return acc;
-}
-
-void DobotM1::checkAlarm_() {
-  // Check Alarm
-  dobot_api::alarmState alarmstate;
-  uint32_t len, maxLen = 32;
-
-  if (dobot_api::GetAlarmsState(alarmstate.value, &len, maxLen) ==
-      dobot_api::DobotConnect_NoError) {
-    publishAlarm_(alarmstate);
-    dobot_api::SetQueuedCmdClear();
-    dobot_api::ClearAllAlarmsState();
-  }
-}
-
-bool DobotM1::assertAlarm_() {
-  dobot_api::alarmState alarmstate;
-  uint32_t len, maxLen = 32;
-  bool state = false;
-  if (dobot_api::GetAlarmsState(alarmstate.value, &len, maxLen) ==
-      dobot_api::DobotConnect_NoError) {
-    u_int32_t code = alarmStateToCode(alarmstate);
-
-    if (code == 0)
-      state = true;
-  }
-  return state;
-}
-
-void DobotM1::publishAlarm_(dobot_api::alarmState alarmstate) {
-  uint32_t code = alarmStateToCode(alarmstate);
-  if (code == 0)
     return;
-  const char *s = dobot_api::getAlartCodeName(dobot_api::AlarmCode(code));
-  ROS_ERROR("%s", s);
-}
+  }
+
+  void DobotM1::timerCallback(const ros::TimerEvent &)
+  {
+    checkAlarm_();
+
+    // Publish Position
+    dobot_api::Pose pose;
+    if (GetPose(&pose) != dobot_api::DobotCommunicate_NoError)
+    {
+      return;
+    };
+
+    sensor_msgs::JointState joint_msg;
+    joint_msg.header.stamp = ros::Time::now();
+    joint_msg.name.resize(4);
+    joint_msg.position.resize(4);
+    joint_msg.name[0] = "joint1";
+    joint_msg.position[0] = (float)pose.jointAngle[0];
+    joint_msg.name[1] = "joint2";
+    joint_msg.position[1] = (float)pose.jointAngle[1];
+    joint_msg.name[2] = "joint3";
+    joint_msg.position[2] = (float)pose.jointAngle[2];
+    joint_msg.name[3] = "joint4";
+    joint_msg.position[3] = (float)pose.jointAngle[3];
+    joint_pub_.publish(joint_msg);
+  }
+
+  void DobotM1::ptpCallback(const m1_msgs::M1Ptp &msg)
+  {
+    uint64_t lastIndex;
+    // set velocity
+    float vel;
+    vel = check_velocity_(msg.vel);
+
+    // set acceleration
+    float acc;
+    acc = check_acceleration_(acc);
+
+    dobot_api::PTPCommonParams ptpCommonParams;
+    ptpCommonParams.velocityRatio = vel;
+    ptpCommonParams.accelerationRatio = acc;
+
+    dobot_api::PTPCmd cmd;
+    cmd.ptpMode = msg.ptpMode;
+    cmd.x = msg.x;
+    cmd.y = msg.y;
+    cmd.z = msg.z;
+    cmd.r = msg.r;
+
+    // Start session with dobot
+    while (
+        !check_communication_(dobot_api::SetArmOrientation(
+                                  dobot_api::LeftyArmOrientation, true, nullptr),
+                              "Set Arm Orientation"))
+    {
+    }
+    while (!check_communication_(
+        SetPTPCommonParams(&ptpCommonParams, true, nullptr), "Set PTP Param"))
+    {
+    }
+    while (!check_communication_(dobot_api::SetPTPCmd(&cmd, true, nullptr),
+                                 "Set PTP Cmd"))
+    {
+    }
+    checkAlarm_();
+  }
+
+  bool DobotM1::PtpServiceCallback_(m1_msgs::M1PtpServiceRequest &req, m1_msgs::M1PtpServiceResponse &res)
+  {
+    // todo
+    return true;
+  }
+
+  void DobotM1::cpCallback(const m1_msgs::M1Cp &msg)
+  {
+    uint64_t lastIndex;
+    // set velocity
+    float vel;
+    vel = check_velocity_(msg.vel);
+
+    // set acceleration
+    float acc;
+    acc = check_acceleration_(acc);
+
+    dobot_api::CPParams cpParams;
+    cpParams.juncitionVel = vel;
+    cpParams.planAcc = acc;
+
+    dobot_api::CPCmd cmd;
+    cmd.cpMode = msg.cpMode;
+    cmd.x = msg.x;
+    cmd.y = msg.y;
+    cmd.z = msg.z;
+
+    // Start session with dobot
+    // Cp command only works on RightyArmOrientation
+    while (
+        !check_communication_(dobot_api::SetArmOrientation(
+                                  dobot_api::RightyArmOrientation, true, nullptr),
+                              "Set Arm Orientation"))
+    {
+    }
+    while (!check_communication_(SetCPParams(&cpParams, true, nullptr),
+                                 "Set CP Param"))
+    {
+    }
+    while (!check_communication_(SetCPCmd(&cmd, true, nullptr), "Set CP Cmd"))
+    {
+    }
+    checkAlarm_();
+  }
+
+  bool DobotM1::CpServiceCallback_(m1_msgs::M1CpServiceRequest &req, m1_msgs::M1CpServiceResponse &res)
+  {
+    // todo
+    return true;
+  }
+
+  void DobotM1::jogCallback(const m1_msgs::M1Jog &msg)
+  {
+    // set velocity
+    float vel;
+    vel = check_velocity_(vel);
+
+    // set acceleration
+    float acc;
+    acc = check_acceleration_(acc);
+
+    dobot_api::JOGCommonParams jogCommonParams;
+    jogCommonParams.velocityRatio = vel;
+    jogCommonParams.accelerationRatio = acc;
+
+    dobot_api::JOGCmd cmd;
+    cmd.isJoint = msg.isJoint;
+    cmd.cmd = msg.jogCmd;
+
+    // Start session with dobot
+    while (!check_communication_(
+        SetJOGCommonParams(&jogCommonParams, true, nullptr), "Set JOG Param"))
+    {
+    }
+    while (!check_communication_(dobot_api::SetJOGCmd(&cmd, false, nullptr),
+                                 "Set JOG Cmd"))
+    {
+    }
+    checkAlarm_();
+  }
+
+  void DobotM1::homing()
+  {
+    // clean queue
+    while (!check_communication_(dobot_api::SetQueuedCmdClear(), "Queue Clear"))
+    {
+    }
+
+    while (!check_communication_(dobot_api::SetQueuedCmdStartExec(),
+                                 "Start Queue"))
+    {
+    }
+
+    uint64_t lastIndex;
+    uint64_t currentIndex;
+
+    while (!check_communication_(
+        dobot_api::SetHOMEWithSwitch(0, true, &lastIndex), "Homing"))
+    {
+    }
+    while (1)
+    {
+      while (dobot_api::GetQueuedCmdCurrentIndex(&currentIndex) !=
+             dobot_api::DobotCommunicate_NoError)
+      {
+      }
+      if (lastIndex <= currentIndex)
+        break;
+    }
+
+    ROS_INFO("Homing Successfully Finished!");
+    return;
+  }
+
+  bool DobotM1::check_connection_(uint8_t status)
+  {
+    switch (status)
+    {
+    case dobot_api::DobotConnect_NoError:
+      return true;
+    case dobot_api::DobotConnect_NotFound:
+      ROS_ERROR("Dobot Connect Not Found");
+      break;
+    case dobot_api::DobotConnect_Occupied:
+      ROS_ERROR("Dobot Connect Occupied");
+      break;
+    default:
+      ROS_ERROR("Unexpected Return");
+      break;
+    }
+    return false;
+  }
+
+  bool DobotM1::check_communication_(uint8_t status, std::string process_name)
+  {
+    switch (status)
+    {
+    case dobot_api::DobotCommunicate_NoError:
+      ROS_INFO("%s", process_name.c_str());
+      return true;
+    case dobot_api::DobotCommunicate_BufferFull:
+      ROS_ERROR("Dobot Communication Buffer Full: %s", process_name.c_str());
+      break;
+    case dobot_api::DobotCommunicate_Timeout:
+      ROS_ERROR("Dobot Communication Timeout: %s", process_name.c_str());
+      break;
+    case dobot_api::DobotCommunicate_InvalidParams:
+      ROS_ERROR("Dobot Communication Invalid Params: %s", process_name.c_str());
+      break;
+    default:
+      ROS_ERROR("Unexpected Return: %s", process_name.c_str());
+      break;
+    }
+    return false;
+  }
+
+  float DobotM1::check_velocity_(float vel)
+  {
+    if (vel < MIN_VEL)
+    {
+      ROS_WARN("velocity modified to minimum value: %f", MAX_VEL);
+      vel = MIN_VEL;
+    }
+    else if (vel > MAX_VEL)
+    {
+      ROS_WARN("velocity modified to maximum value: %f", MAX_VEL);
+      vel = MAX_VEL;
+    }
+    return vel;
+  }
+
+  float DobotM1::check_acceleration_(float acc)
+  {
+    if (acc < MIN_ACC)
+    {
+      ROS_WARN("acceleration modified to minimum value: %f", MIN_ACC);
+      acc = MIN_ACC;
+    }
+    else if (acc > MAX_ACC)
+    {
+      ROS_WARN("acceleration modified to maximum value: %f", MAX_ACC);
+      acc = MAX_ACC;
+    }
+    return acc;
+  }
+
+  void DobotM1::checkAlarm_()
+  {
+    // Check Alarm
+    dobot_api::alarmState alarmstate;
+    uint32_t len, maxLen = 32;
+
+    if (dobot_api::GetAlarmsState(alarmstate.value, &len, maxLen) ==
+        dobot_api::DobotConnect_NoError)
+    {
+      publishAlarm_(alarmstate);
+      dobot_api::SetQueuedCmdClear();
+      dobot_api::ClearAllAlarmsState();
+    }
+  }
+
+  bool DobotM1::assertAlarm_()
+  {
+    dobot_api::alarmState alarmstate;
+    uint32_t len, maxLen = 32;
+    bool state = false;
+    if (dobot_api::GetAlarmsState(alarmstate.value, &len, maxLen) ==
+        dobot_api::DobotConnect_NoError)
+    {
+      u_int32_t code = alarmStateToCode(alarmstate);
+
+      if (code == 0)
+        state = true;
+    }
+    return state;
+  }
+
+  void DobotM1::publishAlarm_(dobot_api::alarmState alarmstate)
+  {
+    uint32_t code = alarmStateToCode(alarmstate);
+    if (code == 0)
+      return;
+    const char *s = dobot_api::getAlartCodeName(dobot_api::AlarmCode(code));
+    ROS_ERROR("%s", s);
+  }
 
 } // namespace dobot_m1
