cf12d4ff569c85f7e079962172fb486cb6cf6eb1
m12watanabe1a <m12watanabe1a@gmail.com>
2021-02-24 23:59:43 +0900

    fix format

--- m1_controller/src/m1_controller.cpp
+++ m1_controller/src/m1_controller.cpp
@@ -1,13 +1,16 @@
 #include <dobot_m1.hpp>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   try
   {
     ros::init(argc, argv, "m1_controller");
     dobot_m1::DobotM1 my_dobot;
     ros::spin();
     return EXIT_SUCCESS;
-  } catch (const std::exception &e) {
+  }
+  catch (const std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
