25349cf71f5d32bfdca92fee5c10f7f18a556d2e
m12watanabe1a <m12watanabe1a@gmail.com>
2021-02-24 23:59:25 +0900

    fix code

--- m1_controller/src/m1_controller.cpp
+++ m1_controller/src/m1_controller.cpp
@@ -1,8 +1,14 @@
 #include <dobot_m1.hpp>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "m1_controller");
-  dobot_m1::DobotM1 my_dobot;
-  ros::spin();
-  return 0;
+  try
+  {
+    ros::init(argc, argv, "m1_controller");
+    dobot_m1::DobotM1 my_dobot;
+    ros::spin();
+    return EXIT_SUCCESS;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
--- m1_controller/src/m1_homing.cpp
+++ m1_controller/src/m1_homing.cpp
@@ -1,8 +1,17 @@
 #include <dobot_m1.hpp>
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "m1_homing");
-  dobot_m1::DobotM1 my_dobot;
-  my_dobot.Homing();
-  return 0;
+int main(int argc, char **argv)
+{
+  try
+  {
+    ros::init(argc, argv, "m1_homing");
+    dobot_m1::DobotM1 my_dobot;
+    my_dobot.Homing();
+    return EXIT_SUCCESS;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
