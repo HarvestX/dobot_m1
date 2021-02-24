832d58c8284929ce1e11e023413af6d5701eb1ee
m12watanabe1a <m12watanabe1a@gmail.com>
2021-02-24 23:47:26 +0900

    fix jog cmd

--- m1_joy/src/joy_commander.cpp
+++ m1_joy/src/joy_commander.cpp
@@ -1,91 +1,129 @@
 #include "joy_commander.h"
 
-namespace dobot_m1 {
-JoyCommander::JoyCommander() : nh_(), pnh_() {
-  pnh_.getParam("isJoint", isJoint_);
-  jog_pub_ = nh_.advertise<m1_msgs::M1Jog>("jog_cmd", 10);
-  joy_sub_ = nh_.subscribe("/joy", 1, &JoyCommander::joyCallback, this);
+namespace dobot_m1
+{
+  JoyCommander::JoyCommander() : nh_(), pnh_()
+  {
+    pnh_.getParam("isJoint", isJoint_);
+    jog_cmd_pub_ = nh_.advertise<m1_msgs::M1JogCmd>("jog_cmd", 1);
+    jog_params_pub_ = nh_.advertise<m1_msgs::M1JogParams>("jog_params", 1);
+    joy_sub_ = nh_.subscribe("/joy", 1, &JoyCommander::joyCallback, this);
 
-  // Initial Jog Command
-  current_jog_msg_.isJoint = isJoint_;
-  current_jog_msg_.jogCmd = 0;
-  current_jog_msg_.vel = 0;
-  current_jog_msg_.acc = 0;
-}
+    // Initial Jog Command
+    current_jog_cmd_msg_.isJoint = isJoint_;
+    current_jog_cmd_msg_.jogCmd = 0;
 
-void JoyCommander::joyCallback(const sensor_msgs::Joy &joy_msg) {
-
-  m1_msgs::M1Jog target_jog_msg;
-  target_jog_msg.jogCmd = 0;
-
-  float x = joy_msg.axes[1];          // L stick horizontal
-  float y = joy_msg.axes[0];          // L stick vertical
-  float z = joy_msg.axes[4];          // R stick vertical
-  float r = joy_msg.axes[3];          // R stick horizontal
-  int R1_buttom = joy_msg.buttons[4]; // R1 button
-
-  if (R1_buttom) {
-    toggleJoyMode();
+    current_jog_params_msg_.vel = 0;
+    current_jog_params_msg_.acc = 0;
   }
 
-  float x_abs = abs(x);
-  float y_abs = abs(y);
-  float z_abs = abs(z);
-  float r_abs = abs(r);
+  void JoyCommander::joyCallback(const sensor_msgs::Joy &joy_msg)
+  {
 
-  float vel = std::max({x_abs, y_abs, z_abs, r_abs});
-  if (vel > 0.4) { // ignore small input
-    if (x_abs >= vel) {
-      if (x > 0) {
-        target_jog_msg.jogCmd = 1; // X+
-      } else {
-        target_jog_msg.jogCmd = 2; // X-
-      }
-    } else if (y_abs >= vel) {
-      if (y > 0) {
-        target_jog_msg.jogCmd = 3; // Y+
-      } else {
-        target_jog_msg.jogCmd = 4; // Y-
-      }
-    } else if (z_abs >= vel) {
-      // Move Joint directly to avoid axis limitation
-      target_jog_msg.isJoint = 1;
-      if (z > 0) {
-        target_jog_msg.jogCmd = 5; // Z+
-      } else {
-        target_jog_msg.jogCmd = 6; // Z-
-      }
-    } else if (r_abs >= vel) {
-      // Move Joint directly to avoid axis limitation
-      target_jog_msg.isJoint = 1;
-      if (r > 0) {
-        target_jog_msg.jogCmd = 7; // R+
+    m1_msgs::M1JogCmd target_jog_cmd_msg;
+    m1_msgs::M1JogParams target_jog_params_msg;
 
-      } else {
-        target_jog_msg.jogCmd = 8; // R-
-      }
+    target_jog_cmd_msg.jogCmd = 0;
+
+    float x = joy_msg.axes[1];          // L stick horizontal
+    float y = joy_msg.axes[0];          // L stick vertical
+    float z = joy_msg.axes[4];          // R stick vertical
+    float r = joy_msg.axes[3];          // R stick horizontal
+    int R1_buttom = joy_msg.buttons[4]; // R1 button
+
+    if (R1_buttom)
+    {
+      toggleJoyMode();
     }
-    target_jog_msg.vel = round(vel * 100.0);
-    target_jog_msg.acc = 100.0;
-  }
 
-  // to nothing
-  if (target_jog_msg == current_jog_msg_) {
-    return;
-  }
+    float x_abs = abs(x);
+    float y_abs = abs(y);
+    float z_abs = abs(z);
+    float r_abs = abs(r);
 
-  // publish command
-  target_jog_msg.isJoint = isJoint_;
-  current_jog_msg_ = target_jog_msg;
-  jog_pub_.publish(target_jog_msg);
-}
+    float vel = std::max({x_abs, y_abs, z_abs, r_abs});
+    if (vel > 0.4)
+    { // ignore small input
+      if (x_abs >= vel)
+      {
+        if (x > 0)
+        {
+          target_jog_cmd_msg.jogCmd = 1; // X+
+        }
+        else
+        {
+          target_jog_cmd_msg.jogCmd = 2; // X-
+        }
+      }
+      else if (y_abs >= vel)
+      {
+        if (y > 0)
+        {
+          target_jog_cmd_msg.jogCmd = 3; // Y+
+        }
+        else
+        {
+          target_jog_cmd_msg.jogCmd = 4; // Y-
+        }
+      }
+      else if (z_abs >= vel)
+      {
+        // Move Joint directly to avoid axis limitation
+        target_jog_cmd_msg.isJoint = 1;
+        if (z > 0)
+        {
+          target_jog_cmd_msg.jogCmd = 5; // Z+
+        }
+        else
+        {
+          target_jog_cmd_msg.jogCmd = 6; // Z-
+        }
+      }
+      else if (r_abs >= vel)
+      {
+        // Move Joint directly to avoid axis limitation
+        target_jog_cmd_msg.isJoint = 1;
+        if (r > 0)
+        {
+          target_jog_cmd_msg.jogCmd = 7; // R+
+        }
+        else
+        {
+          target_jog_cmd_msg.jogCmd = 8; // R-
+        }
+      }
+      target_jog_params_msg.vel = round(vel * 100.0);
+      target_jog_params_msg.acc = 100.0;
+    }
+
+    // parameter
+    if (target_jog_params_msg != current_jog_params_msg_)
+    {
+      // publish command
+      current_jog_params_msg_ = target_jog_params_msg;
+
+      jog_cmd_pub_.publish(target_jog_params_msg);
+    }
+
+    // cmd
+    if (target_jog_cmd_msg != current_jog_cmd_msg_)
+    {
+      // publish command
+      target_jog_cmd_msg.isJoint = isJoint_;
+      current_jog_cmd_msg_ = target_jog_cmd_msg;
+
+      jog_cmd_pub_.publish(target_jog_cmd_msg);
+    }
+  }
 } // namespace dobot_m1
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "joy_commander");
   dobot_m1::JoyCommander joy_commander;
   ros::Rate loop_rate(60);
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     ros::spinOnce();
   }
   return 0;
--- m1_joy/src/joy_commander.h
+++ m1_joy/src/joy_commander.h
@@ -1,29 +1,38 @@
 #include <algorithm>
-#include <m1_msgs/M1Jog.h>
+#include <m1_msgs/M1JogCmd.h>
+#include <m1_msgs/M1JogParams.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 
-namespace dobot_m1 {
-class JoyCommander {
-public:
-  JoyCommander();
-  void joyCallback(const sensor_msgs::Joy &joy_msg);
-  void toggleJoyMode() {
-    isJoint_ = !isJoint_;
-    if (isJoint_) {
-      ROS_WARN("Jog in Joint coordinate system");
-    } else {
-      ROS_WARN("Jog in Cartesian coordinate system");
+namespace dobot_m1
+{
+  class JoyCommander
+  {
+  public:
+    JoyCommander();
+    void joyCallback(const sensor_msgs::Joy &joy_msg);
+    void toggleJoyMode()
+    {
+      isJoint_ = !isJoint_;
+      if (isJoint_)
+      {
+        ROS_WARN("Jog in Joint coordinate system");
+      }
+      else
+      {
+        ROS_WARN("Jog in Cartesian coordinate system");
+      }
+      return;
     }
-    return;
-  }
 
-private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::Publisher jog_pub_;
-  ros::Subscriber joy_sub_;
-  bool isJoint_ = true;
-  m1_msgs::M1Jog current_jog_msg_;
-};
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Publisher jog_cmd_pub_;
+    ros::Publisher jog_params_pub_;
+    ros::Subscriber joy_sub_;
+    bool isJoint_ = true;
+    m1_msgs::M1JogCmd current_jog_cmd_msg_;
+    m1_msgs::M1JogParams current_jog_params_msg_;
+  };
 } // namespace dobot_m1
