c18b05971f69ab333f4902103272e4f7a5ad36f1
m12watanabe1a <m12watanabe1a@gmail.com>
2021-02-25 18:42:41 +0900

    fix

--- m1_controller/src/m1_controller.cpp
+++ m1_controller/src/m1_controller.cpp
@@ -6,12 +6,13 @@ int main(int argc, char **argv)
   {
     ros::init(argc, argv, "m1_controller");
     dobot_m1::DobotM1 my_dobot;
+    ROS_INFO("Dobot M1 Ready");
     ros::spin();
     return EXIT_SUCCESS;
   }
   catch (const std::exception &e)
   {
-    std::cerr << e.what() << std::endl;
+    ROS_ERROR("%s", e.what());
     return EXIT_FAILURE;
   }
 }
--- m1_controller/src/m1_homing.cpp
+++ m1_controller/src/m1_homing.cpp
@@ -6,12 +6,13 @@ int main(int argc, char **argv)
   {
     ros::init(argc, argv, "m1_homing");
     dobot_m1::DobotM1 my_dobot;
+    ROS_INFO("Start Homing...");
     my_dobot.Homing();
     return EXIT_SUCCESS;
   }
   catch (const std::exception &e)
   {
-    std::cerr << e.what() << std::endl;
+    ROS_ERROR("%s", e.what());
     return EXIT_FAILURE;
   }
 }
